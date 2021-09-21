b21bf6c1c7002cc9a2fd10155dba97ef5bc403e0
AmaechiMcMillan <amaechimcmillan@gmail.com>
2021-09-21 09:11:12 -0700

    "staged changes"

--- travelproject/travel/models.py
+++ travelproject/travel/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(max_length=50)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    address = models.CharField(verbose_name="Address", max_length=100, null=True, blank=True)
+    city = models.CharField(verbose_name="City", max_length=100, null=True, blank=True)
+    zip_code = models.CharField(verbose_name="Zip Code", max_length=5, null=True, blank=True)
+    country = models.CharField(verbose_name="Country", max_length=100, null=True, blank=True)
+    signup_confirmation = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def update_profile_signal(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
