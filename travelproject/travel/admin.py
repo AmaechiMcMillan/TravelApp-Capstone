62d6035a5bd96a3177badff595b35d6f7591ce94
AmaechiMcMillan <amaechimcmillan@gmail.com>
2021-09-24 00:12:08 -0700

    "staged files"

--- travelproject/travel/admin.py
+++ travelproject/travel/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Booking, Profile
+from .models import Guest
+from .models import Hotel
+from .models import Room
+from .models import Booking
+
 
 # Register your models here.
 admin.site.register(Profile)
+admin.site.register(Guest)
+admin.site.register(Hotel)
+admin.site.register(Room)
+admin.site.register(Booking)
--- travelproject/travel/models.py
+++ travelproject/travel/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.fields import TimeField
+from datetime import datetime,date,timedelta
+from django.db.models.fields import BLANK_CHOICE_DASH
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -20,7 +21,8 @@ class Profile(models.Model):
         return self.user.username
 
 class Guest(models.Model):
-    name = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     age = models.IntegerField(default=20)
     phone_number = models.CharField(max_length=10)
 
@@ -28,7 +30,8 @@ class Guest(models.Model):
         return self.name
 
 class Hotel(models.Model):
-    name = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=10)
     city= models.CharField(max_length=200)
 
